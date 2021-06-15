6ef7f34254022e30fe384536d001bed883484c0d
Shani Yona <syona@uoguelph.ca>
2021-06-15 22:48:48 +0300

    updated table

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const mysql = require('mysql');
+//create db connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -23,7 +24,7 @@ try {
         if (results.length < 0) {
 
             //create table if none exist
-            connection.query('CREATE TABLE REPO_EVENTS (event_id INT, event_title VARCHAR(20), action VARCHAR(20));');
+            connection.query('CREATE TABLE REPO_EVENTS (event_id INT PRIMARY KEY, event_title VARCHAR(200), action VARCHAR(20), user VARCHAR(100), number_reviewers INT, head_branch VARCHAR(100), base_branch VARCHAR(100));');
         }
     });
 } catch (e) {
@@ -35,13 +36,15 @@ connection.query('DESCRIBE REPO_EVENTS;', ( error, results)=>{
 });
 
 
-
+//listen for requests
 app.listen(444, '0.0.0.0');
+
 app.get('/', (req, res) => {
     res.send('<p>home</p>');
 });
 var jsonParser = bodyParser.json()
 
+//endpoint for github events
 app.post('/payload', jsonParser, (req, res) => {
     data = JSON.stringify(req.body);
     console.log(JSON.parse(data).ref);
