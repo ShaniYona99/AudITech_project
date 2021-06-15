d6230c40d2044f1ef68c2198d6ba7f1565c40431
Shani Yona <syona@uoguelph.ca>
2021-06-16 00:49:51 +0300

    table is populated with pull requests properly

--- server/app.js
+++ server/app.js
@@ -24,16 +24,14 @@ try {
         if (results.length < 0) {
 
             //create table if none exist
-            connection.query('CREATE TABLE REPO_EVENTS (event_id INT PRIMARY KEY, event_title VARCHAR(200), action VARCHAR(20), user VARCHAR(100), number_reviewers INT, head_branch VARCHAR(100), base_branch VARCHAR(100));');
+            connection.query('CREATE TABLE REPO_EVENTS (event_id INT PRIMARY KEY, event_title VARCHAR(200), user VARCHAR(100), head_branch VARCHAR(100), base_branch VARCHAR(100));');
         }
     });
 } catch (e) {
     console.log(`error: ${e}`);
-};
+} 
 
-connection.query('DESCRIBE REPO_EVENTS;', ( error, results)=>{
-    console.log(results);
-});
+
 
 
 //listen for requests
@@ -42,15 +40,28 @@ app.listen(444, '0.0.0.0');
 app.get('/', (req, res) => {
     res.send('<p>home</p>');
 });
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 //endpoint for github events
 app.post('/payload', jsonParser, (req, res) => {
     data = JSON.stringify(req.body);
-    console.log(JSON.parse(data).ref);
+
+    if (JSON.parse(data).pull_request != undefined) {
+        const request = JSON.parse(data).pull_request
+        connection.query(`SELECT event_id FROM mytable WHERE event_id = ${request.id}`, (error, results) => {
+            if(results == 0){
+                connection.query('INSERT INTO REPO_EVENTS (event_id, event_title, user, head_branch, base_branch);' +
+                `VALUES (${request.id}, ${request.title}, ${request.user.login}, ${request.head.ref}, ${request.base.ref});`);
+            }
+        });
+    }
     res.send('s');
-})
+});
 
+let query = 'SELECT * FROM repo_events';
+  
+connection.query(query, (err, rows) => {
 
-
+    console.log(rows);
+});
 
