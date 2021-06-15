e9a44902083b129df3fb30899dab3bf9d896c384
Shani Yona <syona@uoguelph.ca>
2021-06-15 17:00:01 +0300

    new branch

--- server/app.js
+++ server/app.js
@@ -7,13 +7,12 @@ const app = express();
 
 app.listen(444, '0.0.0.0');
 app.get('/', (req, res) =>{
-    res.send('<p>home</p>');
+    res.send('<p>home</p>'); 
 });
 var jsonParser = bodyParser.json()
 
 app.post('/payload',jsonParser, (req, res) => {
     console.log(req.body);
-    res.send('<p>here</p>')
 })
 
 
